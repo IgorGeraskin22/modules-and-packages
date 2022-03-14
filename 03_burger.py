e929c9fcb1d90d02e2094713d28a060110e93806
gin24.ru@yandex.ru <Srv64DfvNs>
2022-03-14 18:46:05 +0500

    03_burger.py

---
+++ 03_burger.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import my_burger
+
+# Создать модуль my_burger. В нем определить функции добавления инградиентов:
+#  - булочки
+#  - котлеты
+#  - огурчика
+#  - помидорчика
+#  - майонеза
+#  - сыра
+# В каждой функции выводить на консоль что-то вроде "А теперь добавим ..."
+
+# В этом модуле создать рецепт двойного чизбургера (https://goo.gl/zA3goZ)
+# с помощью фукций из my_burger и вывести на консоль.
+
+# Создать рецепт своего бургера, по вашему вкусу.
+# Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
+
+# Двойной чизбургер
+my_burger.bun_1()
+my_burger.mayonnaise()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.tomato()
+my_burger.cucumber()
+my_burger.bun_2()
+
+# Двойной чизбургер с беконом
+my_burger.ingredients()
+my_burger.bun_1()
+my_burger.mayonnaise()
+my_burger.salad()
+my_burger.tomato()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.cutlet()
+my_burger.cheese()
+my_burger.bacon()
+my_burger.bun_2()
+
+# Зачёт!
